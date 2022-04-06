f9db83bedc4ca98058ad8776fc9f375ff37fe019
thainvits <thainv@its-global.vn>
2022-04-06 09:09:45 +0700

    update-base

---
+++ resources/js/pages/home/index.js
@@ -0,0 +1,17 @@
+
+import React, { useState, useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+const HomePage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+  },[]);
+
+  return (
+    <>
+      <div>User</div>
+    </>
+  );
+};
+
+export default HomePage;
