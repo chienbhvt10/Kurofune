1d0fbc6a335540fe8e39dc496b7a65d98862d562
thanh-its <thanhvv@its-global.vn>
2022-04-18 15:37:00 +0700

    add build common

---
+++ app/Enums/UserPermission.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class UserPermission extends Enum
+{
+  const PERMISSION_ACCESS_ONLINE_PHARMACY = 'access url online pharmacy';
+  const PERMISSION_ACCESS_CHAT_24H = 'access url chat 24h';
+  const PERMISSION_ACCESS_INCOME = 'access url income';
+  const PERMISSION_ACCESS_OVERSEAS_REMITTANCE = 'access url overseas remittance';
+  const PERMISSION_ACCESS_E_LEANING = 'access url e-learning';
+  const PERMISSION_ACCESS_WABISABI = 'access url wabisabi';
+
+  const PERMISSION_VIEW_PAGE = 'view page';
+  const PERMISSION_VIEW_CATEGORY_PRODUCT = 'view category product';
+  const PERMISSION_VIEW_ORDER_HISTORY = 'view order history';
+  const PERMISSION_VIEW_USER = 'view user';
+  const PERMISSION_UPDATE_USER = 'update user';
+  const PERMISSION_VIEW_PRODUCT = 'view product';
+  const PERMISSION_VIEW_BILLING_ADDRESS = 'view billing address';
+  const PERMISSION_UPDATE_BILLING_ADDRESS = 'update billing address';
+  const PERMISSION_VIEW_SHIPPING_ADDRESS = 'view shipping address';
+  const PERMISSION_UPDATE_SHIPPING_ADDRESS = 'update shipping address';
+
+
+  const PERMISSION_USER_MANAGE = 'manage user';
+  const PERMISSION_VENDOR_MANAGE = 'manage vendor';
+  const PERMISSION_PAGE_MANAGE = 'manage page';
+  const PERMISSION_PRODUCT_CATEGORY_MANAGE = 'manage product category';
+  const PERMISSION_PRODUCT_MANAGE = 'manage product';
+  const PERMISSION_ORDER_MANAGE = 'manage order';
+  const PERMISSION_PERMISSION_MANAGE = 'manage permission';
+  const PERMISSION_BILLING_ADDRESS_MANAGE = 'manage billing address';
+  const PERMISSION_SHIPPING_ADDRESS_MANAGE = 'manage shipping address';
+  const PERMISSION_TAX_MANAGE = 'manage tax';
+  const PERMISSION_SHIPPING_METHOD_MANAGE = 'manage shipping method';
+  const PERMISSION_CART_MANAGE = 'manage cart';
+  const PERMISSION_CHAT_LOG_USER_MANAGE = 'manage chat log user';
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('username');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('active')->comment('Active: 1, Inactive: 0');
+            $table->string('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
