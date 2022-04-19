5adc7688711e2bd4c6358c136af915c0637ca34e
thanh-its <thanhvv@its-global.vn>
2022-04-19 10:28:21 +0700

    add update login

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,57 +4,61 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use http\Env\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     public function login(Request $request) {
-        try {
-            $request->validate([
-                'email' => 'email|required',
-                'password' => 'required'
-            ]);
-
-            $credentials = request(['email', 'password']);
-
-            if (!Auth::attempt($credentials)) {
-                return response()->json([
-                    'status_code' => 500,
-                    'message' => 'Unauthorized'
-                ]);
-            }
-
-            $user = User::where('email', $request->email)->first();
-
-            if (!Hash::check($request->password, $user->password, [])) {
-                throw new \Exception('Error in Login');
-            }
-
-            $tokenResult = $user->createToken('authToken')->plainTextToken;
-
+        $validator = Validator::make($request->all(), [
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+        if ($validator->fails()) {
+            $errors = $validator->errors();
             return response()->json([
-                'status_code' => 200,
-                'access_token' => $tokenResult,
-                'token_type' => 'Bearer',
-            ]);
-        } catch (\Exception $error) {
-            return response()->json([
-                'status_code' => 500,
-                'message' => 'Error in Login',
-                'error' => $error,
-            ]);
+                'status_code' => 422,
+                'message' => __("The given data was invalid."),
+                'errors' => $errors
+            ], 422);
         }
+        $credentials = request(['email', 'password']);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'status_code' => 401,
+                'message' =>  __('auth.failed'),
+            ], 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!Hash::check($request->password, $user->password, [])) {
+            return response([
+                'status_code' => 401,
+                'message' => __('auth.password')
+            ],401);
+        }
+
+        $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json([
+            'status_code' => 200,
+            'access_token' => $tokenResult,
+            'token_type' => 'Bearer',
+        ]);
     }
 
     public function logout(Request $request) {
         $request->user()->tokens()->delete();
-        return response()->json(
-            [
+        return response()->json([
+                'status' => 200,
                 'message' => __('Logged out')
-            ]
-        );
+            ], 200);
     }
 
     public function register(Request $request) {
