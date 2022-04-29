b2288662bf00ed6b5ea2b9889296ffa48a62a6e3
thanh-its <thanhvv@its-global.vn>
2022-04-29 09:39:09 +0700

    Add traits common response form

---
+++ app/Traits/RespondsStatusTrait.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Response;
+
+trait RespondsStatusTrait
+{
+    protected function success($message, $status = Response::HTTP_OK): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'status_code' => $status,
+            'message' => $message,
+        ], $status);
+    }
+
+    protected function successWithData($message, $data = [], $status = Response::HTTP_OK): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'status_code' => $status,
+            'message' => $message,
+            'data' => $data
+        ], $status);
+    }
+
+    protected function responseData($data = [], $status = Response::HTTP_OK): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'status_code' => $status,
+            'data' => $data
+        ], $status);
+    }
+
+    protected function errorResponse($message, $status = Response::HTTP_INTERNAL_SERVER_ERROR): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'status_code' => false,
+            'message' => $message,
+        ], $status);
+    }
+}
