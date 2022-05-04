2f6c1c78f93dd5e5ec5e69d686127fef2e20820f
thanh-its <thanhvv@its-global.vn>
2022-05-04 17:14:46 +0700

    update

--- app/Http/Controllers/API/ChangePasswordController.php
+++ app/Http/Controllers/API/ChangePasswordController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use App\Rules\MatchOldPassword;
 use App\Rules\WithoutSpaces;
 use App\Traits\RespondsStatusTrait;
 use Illuminate\Http\Request;
