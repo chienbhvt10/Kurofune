987f8019ad1e3de66d2ae618f1ebaec4b2217fd1
congmt <congmt@its-global.com>
2022-05-16 17:12:11 +0700

    remove console log

--- resources/js/redux/reducers/authReducers.js
+++ resources/js/redux/reducers/authReducers.js
@@ -13,23 +13,15 @@ const authReducers = createReducer(initialState, (builder) => {
     state.token = actions.payload.access_token;
     state.isLogin = true;
   });
-  builder.addCase(login.rejected, (state, actions) => {
-    console.log("Reject Action");
-  });
+  
   builder.addCase(forgotPassword.fulfilled, (state, actions) => {
     state.response = actions.payload;
   });
-  builder.addCase(forgotPassword.rejected, (state, actions) => {
-    console.log("actions forgot password", actions);
-    console.log("state forgot password", state);
-  });
+  
   builder.addCase(resetPassword.fulfilled, (state, actions) => {
     state.response = actions.payload;
   });
-  builder.addCase(resetPassword.rejected, (state, actions) => {
-    console.log("actions reset password", actions);
-    console.log("state reset password", state);
-  });
+  
   builder.addCase(resetResponseState.fulfilled, (state, actions) => {
     state.response = actions.payload;
   });
