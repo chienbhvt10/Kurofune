b9c443cabd757e00093e11350d4cc1cc82ceddf9
Duong Cong Chien <chiendc@its-global.vn>
2022-05-25 15:08:03 +0700

    create  user pagination

--- resources/js/hooks/user/useUsers.js
+++ resources/js/hooks/user/useUsers.js
@@ -3,15 +3,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { getUsersAction } from "../../redux/actions/userAction";
 
 const useUsers = () => {
-  const { users } = useSelector((state) => state.userState);
+  const userState = useSelector((state) => state.userState);
   const dispatch = useDispatch();
 
-  const getAllUsers = () => {
-    dispatch(getUsersAction());
+  const getAllUsers = (payload) => {
+    dispatch(getUsersAction(payload));
   };
 
   return {
-    users,
+    users: userState.users,
+    pagination: {
+      total: userState.total,
+      from: userState.from,
+      to: userState.to,
+      current_page: userState.current_page,
+      last_page: userState.last_page,
+    },
     getAllUsers,
   };
 };
