97a9001aeb0f70e8f23ad838c34235ea6b4cc363
lamnd <lamnd@its-global.vn>
2022-06-01 17:20:09 +0700

    update detail-pro

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -195,8 +195,38 @@ class ProductController extends Controller
     public function show($id)
     {
         try {
-            $product = Product::find($id);
-            return $this->responseData($product);
+            $data = Product::find($id);
+            $categories = $data->categories;
+            $type = $categories;
+            $translations = $data -> translations;
+            foreach($type as $type){
+                $cat_type = $type->type ;
+            }
+            $response = [
+                'slug' => $data->slug,
+                'sku' => $data->sku,
+                'stock_status' => $data->stock_status,
+                'price' => $this->get_price_html($data->price),
+                'price_tax' =>  $this->get_price_html($this->get_price_including_tax($data)),
+                'status' => $data->status,
+                'product_image' => $data->product_image,
+                'meta_title' => $data->meta_title,
+                'meta_description' => $data->meta_description,
+                'meta_keywords' => $data->meta_keywords,
+                'name' => $data->name,
+                'medicinal_efficacy_classification' => $data->medicinal_efficacy_classification,
+                'features' => $data->features,
+                'precautions' => $data->precautions,
+                'efficacy_effect' => $data->efficacy_effect,
+                'usage_dose' => $data->usage_dose,
+                'active_ingredients' => $data->active_ingredients,
+                'additives' => $data->additives,
+                'precautions_storage_handling' => $data->precautions_storage_handling,
+                'manufacturer' => $data->manufacturer,
+                'type' => CAT_TYPE[$cat_type],
+                'translations' => $translations,
+            ];
+            return $this->responseData($response);
         }catch (\Exception $error){
             return $this->errorResponse($error->getMessage());
         }
@@ -368,10 +398,40 @@ class ProductController extends Controller
     {
         try {
             $data = Product::find($id);
+            $categories = $data->categories;
+            $type = $categories;
+            $translations = $data -> translations;
+            foreach($type as $type){
+                $cat_type = $type->type ;
+            }
+            $response = [
+                'slug' => $data->slug,
+                'sku' => $data->sku,
+                'stock_status' => $data->stock_status,
+                'price' => $this->get_price_html($data->price),
+                'price_tax' =>  $this->get_price_html($this->get_price_including_tax($data)),
+                'status' => $data->status,
+                'product_image' => $data->product_image,
+                'meta_title' => $data->meta_title,
+                'meta_description' => $data->meta_description,
+                'meta_keywords' => $data->meta_keywords,
+                'name' => $data->name,
+                'medicinal_efficacy_classification' => $data->medicinal_efficacy_classification,
+                'features' => $data->features,
+                'precautions' => $data->precautions,
+                'efficacy_effect' => $data->efficacy_effect,
+                'usage_dose' => $data->usage_dose,
+                'active_ingredients' => $data->active_ingredients,
+                'additives' => $data->additives,
+                'precautions_storage_handling' => $data->precautions_storage_handling,
+                'manufacturer' => $data->manufacturer,
+                'type' => CAT_TYPE[$cat_type],
+                'translations' => $translations,
+            ];
             if (empty($data)) {
                 return $this->errorResponse(__('message.product.not_exist'), Response::HTTP_NOT_FOUND);
             }
-            return $this->responseData($data);
+            return $this->responseData($response);
         } catch (\Exception $error) {
             return $this->errorResponse($error->getMessage());
         }
