00869011bc0abe9d79bd92ff494afe59a8776ba8
Duong Cong Chien <chiendc@its-global.vn>
2022-06-06 11:10:28 +0700

    fixbug kurnew 155

--- resources/js/commons/Form/SelectField.jsx
+++ resources/js/commons/Form/SelectField.jsx
@@ -16,33 +16,41 @@ const SelectField = ({
   className,
 }) => {
   const { t } = useTranslation();
+  const getDepend = () => document.querySelector(`#${field}-select`);
   return (
-    <Form.Item
-      className={className}
-      name={field}
-      label={label}
-      labelCol={labelCol}
-      wrapperCol={wrapperCol}
-      rules={rules}
-      help={
-        response?.message?.[error] &&
-        response?.message?.[error].length &&
-        response?.message?.[error][0]
-      }
-      validateStatus={
-        response?.message?.[error] && response?.message?.[error].length
-          ? "error"
-          : ""
-      }
-    >
-      <Select placeholder={placeholder} disabled={disabled} mode={mode}>
-        {options?.map((option, index) => (
-          <Select.Option key={index} value={option.value}>
-            {option.label || t(option.label_translate)}
-          </Select.Option>
-        ))}
-      </Select>
-    </Form.Item>
+    <div id={`${field}-select`} style={{ position: "relative" }}>
+      <Form.Item
+        className={className}
+        name={field}
+        label={label}
+        labelCol={labelCol}
+        wrapperCol={wrapperCol}
+        rules={rules}
+        help={
+          response?.message?.[error] &&
+          response?.message?.[error].length &&
+          response?.message?.[error][0]
+        }
+        validateStatus={
+          response?.message?.[error] && response?.message?.[error].length
+            ? "error"
+            : ""
+        }
+      >
+        <Select
+          placeholder={placeholder}
+          disabled={disabled}
+          mode={mode}
+          getPopupContainer={getDepend}
+        >
+          {options?.map((option, index) => (
+            <Select.Option key={index} value={option.value}>
+              {option.label || t(option.label_translate)}
+            </Select.Option>
+          ))}
+        </Select>
+      </Form.Item>
+    </div>
   );
 };
 
