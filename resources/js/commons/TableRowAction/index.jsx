82ff5a2c9b9e50bb004dc3fb1551870e8714da21
Duong Cong Chien <chiendc@its-global.vn>
2022-06-08 11:39:04 +0700

    fixbug select box

--- resources/js/commons/TableRowAction/index.jsx
+++ resources/js/commons/TableRowAction/index.jsx
@@ -4,7 +4,7 @@ import { Popconfirm } from "antd";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import "./table-row-action.scss";
-const TableRowAction = ({ onDelete, onEdit, record }) => {
+const TableRowAction = ({ onDelete, onEdit, record, confirmLoading }) => {
   const { t } = useTranslation(0);
   return (
     <div className="table-row-action">
@@ -18,6 +18,7 @@ const TableRowAction = ({ onDelete, onEdit, record }) => {
         onConfirm={onDelete(record)}
         okText={t("admins.user.form.option.yes")}
         cancelText={t("admins.user.form.option.no")}
+        okButtonProps={{ loading: confirmLoading }}
       >
         <FontAwesomeIcon icon={faTrashCan} className="img-row" />
       </Popconfirm>
