3744713534adacea031321e9023df54e2cb1f943
hungngo <hungnm@tinhvan.com>
2022-06-09 10:47:03 +0700

    setup redux-persist

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,7 +16,8 @@ import React from "react";
 import { render } from "react-dom";
 import { I18nextProvider } from "react-i18next";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import store, { persistor } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
 import Routes from "./routes/routes";
 import i18n from "./translate/i18n";
 import "antd/dist/antd.css";
@@ -33,9 +34,11 @@ if (
 }
 render(
   <Provider store={store}>
-    <I18nextProvider i18n={i18n}>
-      <Routes />
-    </I18nextProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <I18nextProvider i18n={i18n}>
+        <Routes />
+      </I18nextProvider>
+    </PersistGate>
   </Provider>,
   document.getElementById("app")
 );
