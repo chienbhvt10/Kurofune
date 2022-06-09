ebafdea925d5cd3ae2f7d66e3845acde1e6388d4
Duong Cong Chien <chiendc@its-global.vn>
2022-06-09 16:11:51 +0700

    fixbug kurnew 207 212

--- resources/js/commons/NavigateLink/index.jsx
+++ resources/js/commons/NavigateLink/index.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
-import { getCurrentLanguage } from "../../helper/localStorage";
-import "./navigate-link.scss";
 import { useSelector } from "react-redux";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import { getCurrentLanguage } from "../../helper/localStorage";
 import { isAdmin } from "../../helper/roles";
+import "./navigate-link.scss";
 const NavigateLink = ({ navigateItems, onClick }) => {
   const lang = getCurrentLanguage();
   const { t } = useTranslation();
   const { profile, userInfo } = useSelector((state) => state.authState);
-
-
   return (
     <div className="navbar-main-wrapper">
       <ul className="nav navbar-nav">
@@ -22,14 +20,14 @@ const NavigateLink = ({ navigateItems, onClick }) => {
           )
           .map((item, index) => (
             <li className="menu-item" key={index}>
-              <Link
-                className="nav-link"
+              <NavLink
+                className={`nav-link`}
                 to={`${lang}/${item.link}`}
                 onClick={onClick}
               >
                 <img className="icon" src={item.imageUrl} />
                 <span>{t(item.title)}</span>
-              </Link>
+              </NavLink>
             </li>
           ))}
       </ul>
