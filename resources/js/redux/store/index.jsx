6082db7e79245dd61cc33d8b93776dcc399cbd0e
Duong Cong Chien <chiendc@its-global.vn>
2022-06-09 14:57:10 +0700

    create qa page

--- resources/js/redux/store/index.jsx
+++ resources/js/redux/store/index.jsx
@@ -1,16 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistReducer,persistStore } from 'redux-persist'
-import {combineReducers} from "redux";
-import storage from 'redux-persist/lib/storage'
-import thunk from "redux-thunk";
-import userState from "../reducers/userReducers";
-import productState from "../reducers/productReducers";
 import authState from "../reducers/authReducers";
 import categoryState from "../reducers/categoryReducer";
-import roleState from "../reducers/roleReducers";
 import pharmacyState from "../reducers/pharmacyReducer";
+import productState from "../reducers/productReducers";
+import roleState from "../reducers/roleReducers";
+import userState from "../reducers/userReducers";
 import adminCategoryState from "./../reducers/adminCategoryReducer";
-import logChatState from './../reducers/logChatReducer';
+import logChatState from "./../reducers/logChatReducer";
 
 export const store = configureStore({
   reducer: {
@@ -21,8 +17,8 @@ export const store = configureStore({
     categoryState,
     pharmacyState,
     adminCategoryState,
-    logChatState
+    logChatState,
   },
 });
 
-export default store
+export default store;
