34ea1723bcda712a713688df055b4f7b82c9d7b8
haotm-its <haotm@its-global.vn>
2022-06-15 11:23:00 +0700

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -147,7 +147,7 @@ class User extends Authenticatable
         return $this->hasMany(Order::class, 'user_id', 'id');
     }
 
-    protected static $relations_to_cascade = ['profile', 'address', 'shipping_address', 'billing_address', 'vendor_profile'];
+    protected static $relations_to_cascade = ['profile', 'address', 'shipping_address', 'billing_address', 'vendor_profile', 'cart'];
 
     protected static function boot()
     {
