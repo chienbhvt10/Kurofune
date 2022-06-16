6cd194a14bf6fc329d7b28267e14bc2a06d7bc31
Ngo Hung <hungnm@tinhvan.com>
2022-06-16 11:48:18 +0700

    commit fix remove clg

--- resources/js/hooks/orderAdmin/useGetListOrderAdmin.js
+++ resources/js/hooks/orderAdmin/useGetListOrderAdmin.js
@@ -1,3 +1,5 @@
+import { t } from "i18next";
+import { NotificationError } from "../../commons/Notification/index.jsx";
 import { orderAdminAPI } from "../../services/order-admin-apis/index.js";
 
 const useGetListOrderAdmin = () => {
@@ -12,7 +14,7 @@ const useGetListOrderAdmin = () => {
       let { data } = result
       onSuccess(data)
     }).catch((error) => {
-      console.log('error', error);
+      NotificationError(t("notification"), `Error ${error.message}` );
     })
   };
   return {
