37c6e16a2831b2893ec6f7ac13c8a1520e659a02
do.dinhvan <dodv3006@gmail.com>
2022-06-22 09:43:51 +0700

    fix KURNEW-362

--- resources/js/commons/TableRowAction/index.jsx
+++ resources/js/commons/TableRowAction/index.jsx
@@ -22,19 +22,16 @@ const TableRowAction = ({ onDelete, onEdit, record, confirmLoading, style }) => 
         onClick={onEdit(record)}
         className="img-row"
       />
-      {(!record.hasOwnProperty("allow_deleted") ||
-        (record.hasOwnProperty("allow_deleted") && record?.allow_deleted)) && (
-        <Popconfirm
-          title={t("admins.delete_item_message")}
-          onConfirm={onDelete(record)}
-          okText={t("admins.user.form.option.yes")}
-          cancelText={t("admins.user.form.option.no")}
-          okButtonProps={{ loading: confirmLoading }}
-          getPopupContainer={getDepend}
-        >
-          <FontAwesomeIcon icon={faTrashCan} className="img-row" />
-        </Popconfirm>
-      )}
+      <Popconfirm
+        title={t("admins.delete_item_message")}
+        onConfirm={onDelete(record)}
+        okText={t("admins.user.form.option.yes")}
+        cancelText={t("admins.user.form.option.no")}
+        okButtonProps={{ loading: confirmLoading }}
+        getPopupContainer={getDepend}
+      >
+        <FontAwesomeIcon icon={faTrashCan} className="img-row" />
+      </Popconfirm>
     </div>
   );
 };
