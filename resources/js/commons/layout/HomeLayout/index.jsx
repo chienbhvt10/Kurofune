5633b034eb0032ef4cdb77de5a983c107aac6e27
Ngo Hung <hungnm@tinhvan.com>
2022-06-23 17:04:57 +0700

    fix feature/kurnew-398

--- resources/js/commons/layout/HomeLayout/index.jsx
+++ resources/js/commons/layout/HomeLayout/index.jsx
@@ -4,9 +4,22 @@ import SideBar from "../../../components/SideBar";
 import HeaderHome from "../../../components/HeaderHome";
 import Footer from "../../../components/Footer";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { isAdmin, isVendor } from "../../../helper/checker";
 
 const HomeLayout = ({ styleColor, navigateLinkData }) => {
   const [show, setShow] = React.useState(false);
+  const userInfo = useSelector((state) => state.authState.userInfo);
+  let isShowCart = ()=>{
+    
+    if(isAdmin(userInfo?.roles?.name) ){
+      return false;
+    }
+    if(isVendor(userInfo?.roles?.name) ){
+      return false;
+    }
+    return true
+  }
 
   const toggleSideBar = () => {
     setShow(!show);
@@ -28,7 +41,7 @@ const HomeLayout = ({ styleColor, navigateLinkData }) => {
           styleColor={styleColor}
         />
         <div className="wrap-content">
-          <HeaderHome toggleSideBar={toggleSideBar} />
+          <HeaderHome toggleSideBar={toggleSideBar} isShowCart={isShowCart} />
           <div className="content">
             <Outlet />
           </div>
--- resources/js/components/HeaderHome/index.jsx
+++ resources/js/components/HeaderHome/index.jsx
@@ -9,19 +9,22 @@ import { getCurrentLanguage } from "../../helper/localStorage";
 import "./header-home.scss";
 import useLogout from "../../hooks/auth/useLogout";
 import useCart from "../../hooks/cart/useCart";
-const HeaderHome = ({ toggleSideBar }) => {
+import { isAdmin } from "../../helper/checker";
+const HeaderHome = ({ toggleSideBar,isShowCart }) => {
   const lang = getCurrentLanguage();
   const { t } = useTranslation();
   const { getLogout } = useLogout();
+  // const { profile } = useShowProfile();
+  
   const handleLogout = () => {
     getLogout();
   };
   const { cartInfo, deleteCartItem } = useCart();
   const totalQuantity = cartInfo?.cart_item
     ? cartInfo.cart_item.reduce(
-        (prev, currentItem) => prev + currentItem.quantity,
-        0
-      )
+      (prev, currentItem) => prev + currentItem.quantity,
+      0
+    )
     : 0;
   return (
     <div id="header-home">
@@ -52,97 +55,100 @@ const HeaderHome = ({ toggleSideBar }) => {
             title={t("header.btn_back3")}
           />
           <div className="block-profile-header ">
-            <div className="shopping-cart">
-              <div className="icon-cart">
-                <Link
-                  id="cart-custom"
-                  to={`${lang}/cart`}
-                  title={t("header.cart_title")}
-                >
-                  <img className="icon" src="/images/icon-card.png" />
-                  {Number(totalQuantity) > 0 && (
-                    <span className="quantity">{totalQuantity}</span>
-                  )}
-                </Link>
-              </div>
 
-              <div className="mini-cart">
-                <div className="basket-block">
-                  <div className="cart_block_list">
-                    {cartInfo?.cart_item.length > 0 ? (
-                      <div className="group">
-                        <p className="buttons">
-                          <Link
-                            to={`${lang}/cart`}
-                            className="button wc-forward"
-                          >
-                            {t("header.btn_view_cart")}
-                          </Link>{" "}
-                        </p>
-
-                        <ul className="cart_list product_list_widget ">
-                          {cartInfo.cart_item.map((item) => (
-                            <li className="mini_cart_item" key={item.id}>
-                              <button
-                                className="remove remove_from_cart_button_cus"
-                                aria-label="Xóa sản phẩm này"
-                                data-product_id="851"
-                                data-item-key="92fb0c6d1758261f10d052e6e2c1123c"
-                                data-product_sku=""
-                                onClick={() => deleteCartItem(item.id)}
-                              >
-                                ×
-                              </button>{" "}
-                              <span className="quantity">
-                                {item?.quantity} ×{" "}
-                                <span className="amount">
-                                  <bdi>
-                                    {item?.price_tax.toFixed(3)}&nbsp;
-                                    <span>(JPY)</span>
-                                  </bdi>
-                                </span>
-                              </span>{" "}
-                              <Link
-                                to={`${lang}/product-detail/${item?.id}`}
-                                className="item-img"
-                              >
-                                <img
-                                  alt="image-prod-Medicine list"
-                                  src={item.product_image}
-                                />
-                              </Link>
-                            </li>
-                          ))}
-                        </ul>
-                        <p className="total d-none">
-                          <strong>Tổng số phụ:</strong>{" "}
-                          <span className="">
-                            <bdi>
-                              878&nbsp;
-                              <span className="">(JPY)</span>
-                            </bdi>
-                          </span>{" "}
-                          <small className="tax_label">(Bao gồm thuế)</small>{" "}
-                        </p>
-
-                        <p className="buttons checkout-btn">
-                          <Link
-                            to={`${lang}/checkout`}
-                            className="button checkout wc-forward"
-                          >
-                            {t("header.btn_buy")}
-                          </Link>{" "}
-                        </p>
-                      </div>
-                    ) : (
-                      <p className="empty-cart-text">
-                        {t("header.cart_empty")}
-                      </p>
+            {isShowCart() &&
+              <div className="shopping-cart">
+                <div className="icon-cart">
+                  <Link
+                    id="cart-custom"
+                    to={`${lang}/cart`}
+                    title={t("header.cart_title")}
+                  >
+                    <img className="icon" src="/images/icon-card.png" />
+                    {Number(totalQuantity) > 0 && (
+                      <span className="quantity">{totalQuantity}</span>
                     )}
+                  </Link>
+                </div>
+
+                <div className="mini-cart">
+                  <div className="basket-block">
+                    <div className="cart_block_list">
+                      {cartInfo?.cart_item.length > 0 ? (
+                        <div className="group">
+                          <p className="buttons">
+                            <Link
+                              to={`${lang}/cart`}
+                              className="button wc-forward"
+                            >
+                              {t("header.btn_view_cart")}
+                            </Link>{" "}
+                          </p>
+
+                          <ul className="cart_list product_list_widget ">
+                            {cartInfo.cart_item.map((item) => (
+                              <li className="mini_cart_item" key={item.id}>
+                                <button
+                                  className="remove remove_from_cart_button_cus"
+                                  aria-label="Xóa sản phẩm này"
+                                  data-product_id="851"
+                                  data-item-key="92fb0c6d1758261f10d052e6e2c1123c"
+                                  data-product_sku=""
+                                  onClick={() => deleteCartItem(item.id)}
+                                >
+                                  ×
+                                </button>{" "}
+                                <span className="quantity">
+                                  {item?.quantity} ×{" "}
+                                  <span className="amount">
+                                    <bdi>
+                                      {item?.price_tax.toFixed(3)}&nbsp;
+                                      <span>(JPY)</span>
+                                    </bdi>
+                                  </span>
+                                </span>{" "}
+                                <Link
+                                  to={`${lang}/product-detail/${item?.id}`}
+                                  className="item-img"
+                                >
+                                  <img
+                                    alt="image-prod-Medicine list"
+                                    src={item.product_image}
+                                  />
+                                </Link>
+                              </li>
+                            ))}
+                          </ul>
+                          <p className="total d-none">
+                            <strong>Tổng số phụ:</strong>{" "}
+                            <span className="">
+                              <bdi>
+                                878&nbsp;
+                                <span className="">(JPY)</span>
+                              </bdi>
+                            </span>{" "}
+                            <small className="tax_label">(Bao gồm thuế)</small>{" "}
+                          </p>
+
+                          <p className="buttons checkout-btn">
+                            <Link
+                              to={`${lang}/checkout`}
+                              className="button checkout wc-forward"
+                            >
+                              {t("header.btn_buy")}
+                            </Link>{" "}
+                          </p>
+                        </div>
+                      ) : (
+                        <p className="empty-cart-text">
+                          {t("header.cart_empty")}
+                        </p>
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            }
 
             <div className="language-switcher language-switcher-dropdown">
               <Languages />
