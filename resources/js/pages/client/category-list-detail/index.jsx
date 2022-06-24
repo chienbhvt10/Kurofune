bb318718961020d0cf970bab3c235c903392cc7f
tiendc1 <tiendc@its-global.vn>
2022-06-24 09:15:58 +0700

    fix

--- resources/js/pages/client/category-list-detail/index.jsx
+++ resources/js/pages/client/category-list-detail/index.jsx
@@ -37,7 +37,7 @@ const CategoryListDetail = () => {
               }
             </div>
           </div>
-          <CardCategoryDetail cardItems={category?.product} />
+          <CardCategoryDetail cardItems={category?.products} />
         </div>
       </div>
     </>
