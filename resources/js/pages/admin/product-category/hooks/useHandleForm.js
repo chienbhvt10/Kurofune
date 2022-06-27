a6639c6ffdb24e0ade828eb45ee40526fb3fc2c1
Duong Cong Chien <chiendc@its-global.vn>
2022-06-27 14:40:48 +0700

    fixbug submit form category

--- resources/js/pages/admin/product-category/hooks/useHandleForm.js
+++ resources/js/pages/admin/product-category/hooks/useHandleForm.js
@@ -1,65 +1,75 @@
 import { Form } from "antd";
 import React from "react";
 import { TYPE_FORM_UPDATE } from "../../../../constants";
-import { appendObjectToFormData } from "../../../../helper/handler";
+import {
+  appendObjectToFormData,
+  getResultValidate,
+} from "../../../../helper/handler";
 import { getCurrentLanguage } from "../../../../helper/localStorage";
 import { getCategoryInitValues } from "../category-form/categoryInitValues";
 import useHandleImage from "./useHandleImage";
 import useHandleTranslateForm from "./useHandleTranslateForm";
 
 const useHandleForm = (item, onSave, typeForm) => {
-  const formData = new FormData();
   const [isSubmitted, setSubmitted] = React.useState(false);
-
+  const [categoryForm] = Form.useForm();
+  const lang = getCurrentLanguage();
+  const initialCommonValues = getCategoryInitValues(item);
+  const { avatar, onChangeAvatar, errorMessImage, setErrorMessImage } =
+    useHandleImage();
   const {
     categoryProfileFormEN,
     categoryProfileFormJP,
     categoryProfileFormTL,
     categoryProfileFormVI,
     categoryProfileFormZH,
+    validateTranslateForm,
   } = useHandleTranslateForm(item);
-  const lang = getCurrentLanguage();
-  const { avatar, onChangeAvatar, errorMessImage, setErrorMessImage } =
-    useHandleImage();
 
-  const initialCommonValues = getCategoryInitValues(item);
-  const [categoryForm] = Form.useForm();
+  const onFinishAll = async () => {
+    const formData = new FormData();
+    const enFormValues = await getResultValidate(categoryProfileFormEN);
+    const jaFormValues = await getResultValidate(categoryProfileFormJP);
+    const tlFormValues = await getResultValidate(categoryProfileFormTL);
+    const viFormValues = await getResultValidate(categoryProfileFormVI);
+    const zhFormValues = await getResultValidate(categoryProfileFormZH);
 
-  const onFinishAll = () => {
-    let submitValues = {
-      id: initialCommonValues.id,
-      category_image: avatar,
-      ...categoryForm.getFieldsValue(),
-    };
-    appendObjectToFormData(formData, categoryProfileFormEN.getFieldsValue());
-    appendObjectToFormData(formData, categoryProfileFormJP.getFieldsValue());
-    appendObjectToFormData(formData, categoryProfileFormTL.getFieldsValue());
-    appendObjectToFormData(formData, categoryProfileFormVI.getFieldsValue());
-    appendObjectToFormData(formData, categoryProfileFormZH.getFieldsValue());
-
-    if (!avatar) {
-      delete submitValues.category_image;
-    }
-    if (!categoryForm.getFieldValue("parent_id")) {
-      delete submitValues.parent_id;
-    }
     if (typeForm === TYPE_FORM_UPDATE) {
       setErrorMessImage("");
       formData.append("_method", "PUT");
     } else {
       setErrorMessImage(!avatar);
     }
+
+    if (
+      enFormValues.errorFields ||
+      jaFormValues.errorFields ||
+      tlFormValues.errorFields ||
+      viFormValues.errorFields ||
+      zhFormValues.errorFields ||
+      !avatar
+    ) {
+      return;
+    } else {
+      let submitValues = {
+        id: initialCommonValues.id,
+        category_image: avatar,
+        ...categoryForm.getFieldsValue(),
+      };
+
+      appendObjectToFormData(formData, submitValues);
+      appendObjectToFormData(formData, enFormValues);
+      appendObjectToFormData(formData, jaFormValues);
+      appendObjectToFormData(formData, tlFormValues);
+      appendObjectToFormData(formData, viFormValues);
+      appendObjectToFormData(formData, zhFormValues);
+      onSave(formData);
+    }
     setSubmitted(true);
-    appendObjectToFormData(formData, submitValues);
-    onSave(formData);
   };
 
   const onFinishError = () => {
-    categoryProfileFormJP.validateFields();
-    categoryProfileFormVI.validateFields();
-    categoryProfileFormTL.validateFields();
-    categoryProfileFormZH.validateFields();
-    categoryProfileFormEN.validateFields();
+    validateTranslateForm();
     setErrorMessImage(!avatar);
     setSubmitted(true);
   };
@@ -71,11 +81,7 @@ const useHandleForm = (item, onSave, typeForm) => {
   React.useEffect(() => {
     if (isSubmitted) {
       categoryForm.validateFields();
-      categoryProfileFormEN.validateFields();
-      categoryProfileFormJP.validateFields();
-      categoryProfileFormTL.validateFields();
-      categoryProfileFormVI.validateFields();
-      categoryProfileFormZH.validateFields();
+      validateTranslateForm();
     }
   }, [lang]);
 
