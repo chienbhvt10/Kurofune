edbcbc072f407f965a4ad610a419553514bc2556
Duong Cong Chien <chiendc@its-global.vn>
2022-06-30 16:11:56 +0700

    fixbug/kurnew475

--- resources/js/commons/TableHeader/index.jsx
+++ resources/js/commons/TableHeader/index.jsx
@@ -1,11 +1,12 @@
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faDownload, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, Col, Form, Input, Row } from "antd";
-import React from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
+import UploadCsv from "../UploadCsv/UploadCsv";
 import { Breadcrumb } from "../../commons/Breadcrumb";
 import "./table-header.scss";
+
 export const TableHeader = ({
   children,
   title,
@@ -17,6 +18,10 @@ export const TableHeader = ({
   onChangeSearch,
   onResetFilter,
   showReset,
+  exportCsv,
+  importCsv,
+  onExportCsv,
+  onImportCsv,
 }) => {
   const { t } = useTranslation();
   const navigate = useNavigate();
@@ -31,61 +36,87 @@ export const TableHeader = ({
         <Breadcrumb title={title} breadcrumb={breadcrumb} />
       </Col>
       <Col span={24}>
-        <Row justify="space-between">
-          <Col>
-            <Row align="middle">
+        <Row align="middle" gutter={[0, 10]}>
+          <Col span={24}>
+            <Row gutter={[10, 10]} justify="space-between">
               <Col>
-                {addLink ? (
-                  <Button type="primary" onClick={onNavigateCreate}>
-                    <FontAwesomeIcon className="mr-1" icon={faPlus} />
-                    {t("admins.btn_add_new")}
-                  </Button>
-                ) : (
-                  <></>
-                )}
+                <Row gutter={[10, 10]}>
+                  <Col>
+                    {addLink ? (
+                      <Button type="primary" onClick={onNavigateCreate}>
+                        <FontAwesomeIcon className="mr-1" icon={faPlus} />
+                        {t("admins.btn_add_new")}
+                      </Button>
+                    ) : (
+                      <></>
+                    )}
+                  </Col>
+                  <Col>
+                    {showReset ? (
+                      <Button
+                        className="btn-reset"
+                        type="primary"
+                        onClick={onResetFilter}
+                      >
+                        {t("admins.btn_reset")}
+                      </Button>
+                    ) : (
+                      ""
+                    )}
+                  </Col>
+                  {exportCsv && (
+                    <Col>
+                      <Button
+                        className="btn-export"
+                        type="primary"
+                        onClick={onExportCsv}
+                      >
+                        <FontAwesomeIcon
+                          icon={faDownload}
+                          className="mr-2"
+                          style={{ color: "white" }}
+                        />
+                        <span>{t("admins.log_chatbot.btn_export")}</span>
+                      </Button>
+                    </Col>
+                  )}
+                  {importCsv && (
+                    <Col>
+                      <UploadCsv onChangeFile={onImportCsv} />
+                    </Col>
+                  )}
+                </Row>
               </Col>
-              <Col>
-                {showReset ? (
-                  <Button
-                    className="btn-reset"
-                    type="primary"
-                    onClick={onResetFilter}
-                  >
-                    {t("admins.btn_reset")}
-                  </Button>
-                ) : (
-                  ""
-                )}
-              </Col>
-              <Col className="filter">{children}</Col>
+              {searchField && (
+                <Col>
+                  <Form onFinish={onSearch} autoComplete="off">
+                    <Row align="middle" gutter={[10, 10]}>
+                      <Col>
+                        <Form.Item name={searchField} className="search-field">
+                          <Input
+                            type="text"
+                            placeholder={searchPlaceHolder}
+                            onChange={onChangeSearch}
+                          />
+                        </Form.Item>
+                      </Col>
+                      <Col>
+                        <Button
+                          className="btn-search"
+                          type="primary"
+                          htmlType="submit"
+                        >
+                          {t("admins.btn_search")}
+                        </Button>
+                      </Col>
+                    </Row>
+                  </Form>
+                </Col>
+              )}
             </Row>
           </Col>
-          {searchField && (
-            <Col>
-              <Form onFinish={onSearch} autoComplete="off">
-                <Row align="middle">
-                  <Col>
-                    <Form.Item name={searchField} className="search-field">
-                      <Input
-                        type="text"
-                        placeholder={searchPlaceHolder}
-                        onChange={onChangeSearch}
-                      />
-                    </Form.Item>
-                  </Col>
-                  <Col>
-                    <Button
-                      className="btn-search"
-                      type="primary"
-                      htmlType="submit"
-                    >
-                      {t("admins.btn_search")}
-                    </Button>
-                  </Col>
-                </Row>
-              </Form>
-            </Col>
-          )}
+
+          <Col>{children}</Col>
         </Row>
       </Col>
     </Row>
--- resources/js/components/Modal/ModalAccessRight.jsx
+++ resources/js/components/Modal/ModalAccessRight.jsx
@@ -1,38 +1,51 @@
-import { Button, Modal } from 'antd';
-import { t } from 'i18next';
-import React, { useState } from 'react';
-import useUserRegistrationClient from '../../hooks/user/useUserRegistrationClient';
-import { TheServices } from './data';
+import { Modal } from "antd";
+import { t } from "i18next";
+import React from "react";
+import useUserRegistrationClient from "../../hooks/user/useUserRegistrationClient";
+import { TheServices } from "./data";
 import "./modal-custom.scss";
 const ModalAccessRight = ({ modalVisible, setModalVisible, role, profile }) => {
-  const { aviableCounterClient } = useUserRegistrationClient()
+  const { availableCounterClient } = useUserRegistrationClient();
   React.useEffect(() => {
-    aviableCounterClient();
-  }, [])
+    availableCounterClient();
+  }, []);
   return (
     <Modal
       visible={modalVisible}
       footer={null}
       onCancel={() => {
-        setModalVisible(false)
+        setModalVisible(false);
       }}
       className="modal-access-right"
     >
       <div className="modal-custom">
         <div className="modal-custom-body">
           <div className="modal-custom-content">
-            <img className="modal-custom-image" src='/images/people.png' />
-            <p className="modal-custom-description">{t(`client.media.light_plan.modal_access_right.description`,{ payload: profile.profile.company_name })}</p>
-            <p className="modal-custom-title">{t(`client.media.light_plan.modal_access_right.title`, { payload: profile.name })}</p>
-            <ul className="modal-custom-list">
-              {TheServices[role] && TheServices[role].map((content, index) => {
-                return (
-                  <li className="modal-custom-item" key={index}>
-                    <span className="modal-custom-item-index">{index + 1}</span>
-                    <span className="modal-custom-item-content">{t(content)}</span>
-                  </li>
-                )
+            <img className="modal-custom-image" src="/images/people.png" />
+            <p className="modal-custom-description">
+              {t(`client.media.light_plan.modal_access_right.description`, {
+                payload: profile.profile.company_name,
               })}
+            </p>
+            <p className="modal-custom-title">
+              {t(`client.media.light_plan.modal_access_right.title`, {
+                payload: profile.name,
+              })}
+            </p>
+            <ul className="modal-custom-list">
+              {TheServices[role] &&
+                TheServices[role].map((content, index) => {
+                  return (
+                    <li className="modal-custom-item" key={index}>
+                      <span className="modal-custom-item-index">
+                        {index + 1}
+                      </span>
+                      <span className="modal-custom-item-content">
+                        {t(content)}
+                      </span>
+                    </li>
+                  );
+                })}
             </ul>
           </div>
         </div>
